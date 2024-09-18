2b25d5be5751b86d94eacff8f0b5265413b22a9e
eremanikita <eremanikita@gmail.com>
2024-09-18 12:00:00 +0300

    homework-03 v3

--- 20240917/1/prog.py
+++ 20240917/1/prog.py
@@ -1,2 +1,2 @@
 number = int(input())
-print(f"A {"+" if number % 2 == 0 and number % 25 == 0 else "-"} B {"+" if number % 2 != 0 and number % 25 == 0 else "-"} C {"+" if number % 8 == 0  else "-"}")
+print(f"A {"+" if number % 2 == 0 and number % 25 == 0 else "-"} B {"+" if number % 2 and number % 25 == 0 else "-"} C {"+" if number % 8 == 0  else "-"}")
