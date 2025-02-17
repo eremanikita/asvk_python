cea7ff87fb1fcf603e33c10178231e79656f0f2d
eremanikita <eremanikita@gmail.com>
2025-02-17 21:24:28 +0300

    homework2-01 second part
    Show last commit in the branch

--- 20250217/1/prog.py
+++ 20250217/1/prog.py
@@ -1,10 +1,25 @@
+import zlib
 from sys import argv
 from glob import iglob
 from os.path import basename
+from pathlib import Path
 
 
 def find_branches(repo_link):
     return iglob(repo_link + "/refs/heads/*")
+
+
+def get_last_commit_hash(branches_list, branch_name):
+    for branch in branches_list:
+        if basename(branch) == branch_name:
+            required_branch = branch
+            break
+    else:
+        print(f"Не найдено ветки {branch_name}.")
+        exit()
+
+    with open(required_branch, "r") as f:
+        return f.read().rstrip()
 
 
 match len(argv):
@@ -13,3 +28,11 @@ match len(argv):
     case 2:
         for branch in find_branches(argv[1]):
             print(basename(branch))
+    case 3:
+        last_commit = get_last_commit_hash(find_branches(argv[1]), argv[2])
+
+        commit_object = zlib.decompress(
+            Path(argv[1] + f"/objects/{last_commit[:2]}/{last_commit[2:]}").read_bytes())
+        head, _, blob = commit_object.partition(b'\x00')
+        blob = blob.decode()
+        print(blob)
