182395b48eae093bc0d8f8ca7ccb111550532f7c
eremanikita <eremanikita@gmail.com>
2025-02-17 21:22:13 +0300

    homework2-01 first part v2
    Extract method

--- 20250217/1/prog.py
+++ 20250217/1/prog.py
@@ -2,9 +2,14 @@ from sys import argv
 from glob import iglob
 from os.path import basename
 
+
+def find_branches(repo_link):
+    return iglob(repo_link + "/refs/heads/*")
+
+
 match len(argv):
     case 1:
         print("Ошибка ввода параметров. Должен быть передан путь к репозиторию.")
     case 2:
-        for i in iglob(argv[1] + "/refs/heads/*"):
-            print(basename(i))
+        for branch in find_branches(argv[1]):
+            print(basename(branch))
