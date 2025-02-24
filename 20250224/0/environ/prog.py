f82aedeee858af93b71f1c68cc3d033b6d93160e
eremanikita <eremanikita@gmail.com>
2025-02-24 14:31:22 +0300

    sem2-02 v2

--- 20250224/0/environ/prog.py
+++ 20250224/0/environ/prog.py
@@ -1,5 +1,11 @@
 import sys
 import cowsay
+import glob
 
-if "/" in
-print(cowsay.cowsay("QQ", "dragon"))
+files = glob.glob("*.cow")
+for i in files:
+    tmp = input()
+    with open(i, "r") as f:
+        the_cow = cowsay.read_dot_cow(f)
+
+    print(cowsay.cowsay("QQ", cowfile=the_cow))
