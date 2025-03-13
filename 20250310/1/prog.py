875a4d73dfc8b9b0227b53d5f29d0a5a5a156232
eremanikita <eremanikita@gmail.com>
2025-03-13 13:16:50 +0300

    copy last solution

---
+++ 20250310/1/prog.py
@@ -0,0 +1,130 @@
+from cowsay import cowsay, list_cows, read_dot_cow
+from io import StringIO
+import shlex
+
+
+class Cow:
+    custom_cows = {
+        "jgsbat": """    ,_                    _,
+    ) '-._  ,_    _,  _.-' (
+    )  _.-'.|\\\\--//|.'-._  (
+     )'   .'\\/o\\/o\\/'.   `(
+      ) .' . \\====/ . '. (
+       )  / <<    >> \\  (
+        '-._/``  ``\\_.-'
+  jgs     __\\\\'--'//__
+         (((""`  `"")))"""
+    }
+
+
+class Mob:
+    def __init__(self, message, name, hp):
+        self.message = message
+        self.name = name
+        self.hp = hp
+
+    def say(self):
+        if self.name in list_cows():
+            print(cowsay(self.message, cow=self.name))
+        else:
+            print(cowsay(self.message, cowfile=read_dot_cow(StringIO(Cow.custom_cows[self.name]))))
+
+    @staticmethod
+    def check_name(name):
+        return name in list_cows() or name in Cow.custom_cows
+
+
+class Field:
+    def __init__(self):
+        self.field = [[None for _ in range(10)] for _ in range(10)]
+
+    def add_mob(self, cord, name, message, hp):
+        if Mob.check_name(name):
+            self.field[cord.x][cord.y] = Mob(message, name, hp)
+            print(f"Added monster {name} to ({cord.x}, {cord.y}) saying {message}")
+        else:
+            print("Cannot add unknown monster")
+
+
+class Cord:
+    def __init__(self, x=0, y=0):
+        self.x: int = x
+        self.y: int = y
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+
+class Player:
+    def __init__(self):
+        self.cord = Cord()
+
+    def move(self, direction):
+        match direction:
+            case "up":
+                self.cord = Cord(self.cord.x, (self.cord.y - 1) % 10)
+            case "down":
+                self.cord = Cord(self.cord.x, (self.cord.y + 1) % 10)
+            case "left":
+                self.cord = Cord((self.cord.x - 1) % 10, self.cord.y)
+            case "right":
+                self.cord = Cord((self.cord.x + 1) % 10, self.cord.y)
+        print(f"Moved to {self.cord}")
+
+
+class Game:
+    def __init__(self):
+        print("<<< Welcome to Python-MUD 0.1 >>>")
+        self.field = Field()
+        self.player = Player()
+
+    def add_mob(self, cord: Cord, name: str, message: str, hp: int) -> None:
+        self.field.add_mob(cord, name, message, hp)
+
+    def encounter(self):
+        if self.field.field[self.player.cord.x][self.player.cord.y] is not None:
+            self.field.field[self.player.cord.x][self.player.cord.y].say()
+
+    def move_player(self, direction):
+        self.player.move(direction)
+        self.encounter()
+
+
+def main():
+    required_keys = {"name", "hello", "hp", "coords"}
+    game = Game()
+    while line := input():
+        tokens = shlex.split(line)
+        match tokens[0]:
+            case "up" | "down" | "left" | "right" as direction:
+                game.move_player(direction)
+            case "addmob" if len(tokens) > 2:
+
+                params = {"name": tokens[1]}
+                i = 2
+                token_length = len(tokens)
+                while i < token_length:
+                    if tokens[i] == "hello":
+                        if i + 1 < token_length:
+                            params["hello"] = tokens[i + 1]
+                        i += 2
+                    elif tokens[i] == "hp":
+                        if i + 1 < len(tokens) and tokens[i + 1].isdigit() and int(tokens[i + 1]) > 0:
+                            params["hp"] = int(tokens[i + 1])
+                        i += 2
+                    elif tokens[i] == "coords":
+                        if i + 2 < len(tokens) and tokens[i + 1].isdigit() and tokens[i + 2].isdigit():
+                            params["coords"] = Cord(int(tokens[i + 1]), int(tokens[i + 2]))
+                        i += 3
+
+                if not (required_keys - params.keys()):
+                    game.add_mob(params["coords"], params["name"], params["hello"], params["hp"])
+                else:
+                    print("Invalid command")
+
+            case _:
+                print("Invalid command")
+
+
+if __name__ == "__main__":
+    main()
